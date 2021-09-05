aa577fbe5fa6d95bd29f651611b0fb0ff7df874c
IgorVolokhov <volohkov9@gmail.com>
2021-09-05 18:10:31 +0300

    added bcrypt to db

--- server/DAL/repository/login.db.js
+++ server/DAL/repository/login.db.js
@@ -1,31 +1,54 @@
-function addUserDb(db, username, password, email) {
+const bcrypt = require("bcrypt");
+
+// TODO come back to -1
+
+async function comparePasswords(password, bcryptPassword) {
+  return await bcrypt.compare(password, bcryptPassword);
+}
+
+async function addUserDb(db, username, password, email) {
   const users = db.get("users").value();
 
-  if (
-    users.find(
-      (user) =>
-        user.password === password ||
-        user.username === username ||
-        user.email === email
-    )
-  )
-    return false;
+  let user = "";
+  let doesPasswordExist = false;
+  for (let i = 0; i < users.length; i++) {
+    user = users[i];
+    doesPasswordExist = await comparePasswords(password, user.password);
+    if (
+      doesPasswordExist ||
+      user.username === username ||
+      user.email === email
+    ) {
+      return false;
+    }
+  }
 
-  users.push({ username: username, password: password, email: email });
-
+  try {
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+    users.push({ username: username, password: hashedPassword, email: email });
+  } catch {
+    console.log("error");
+    return -1;
+  }
   db.write();
 
   return true;
 }
 
-function checkIfUserExistsDb(db, username, password) {
+async function checkIfUserExistsDb(db, username, password) {
   const users = db.get("users").value();
 
-  const doesExist = users.find(
-    (user) => user.username === username && user.password === password
-  );
+  let doesPasswordExist = false;
+  for (let i = 0; i < users.length; i++) {
+    user = users[i];
+    doesPasswordExist = await comparePasswords(password, user.password);
+    if (doesPasswordExist || user.username === username) {
+      return true;
+    }
+  }
 
-  return doesExist ? true : false;
+  return false;
 }
 
 module.exports = {
--- server/api/controllers/login.js
+++ server/api/controllers/login.js
@@ -1,17 +1,17 @@
 const { addUser, checkIfUserExists } = require("../../DAL/db");
 
 module.exports = {
-  signup: (req, res) => {
+  signup: async (req, res) => {
     const { username, password, email } = req.body;
-    const isAdded = addUser(username, password, email);
+    const isAdded = await addUser(username, password, email);
 
     res.status(200).json({
       message: isAdded ? `added successfully` : `did not add :(`,
     });
   },
-  login: (req, res) => {
+  login: async (req, res) => {
     const { username, password, email } = req.body;
-    const doesExist = checkIfUserExists(username, password);
+    const doesExist = await checkIfUserExists(username, password);
 
     res.status(200).json({
       message: doesExist ? `you can login in` : `does not try again!`,
