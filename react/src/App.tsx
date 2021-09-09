7d3f54ff5646f82526118cb2abbd8c81825ecd57
Liel <lielarie123@me.com>
2021-09-09 13:54:51 +0300

    liel1

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,10 +1,23 @@
-import './App.css';
+import { Route, Router, Switch, BrowserRouter } from "react-router-dom";
+import "./App.css";
+import Login from "./pages/Login/Login";
+import SignUp from "./pages/SignUp/SignUp";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello
-      </header>
+      <BrowserRouter>
+        <div>
+          <Switch>
+            <Route exact path="/">
+              <Login />
+            </Route>
+            <Route path="/signup">
+              <SignUp />
+            </Route>
+          </Switch>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
--- react/src/pages/SignUp/SignUp.tsx
+++ react/src/pages/SignUp/SignUp.tsx
@@ -1,9 +1,138 @@
-const SignUp = () => {
-    return (
-        <div>
-
-        </div>
-    )
+import React, { useState } from "react";
+import axios from "axios";
+const baseURL = "http://localhost:300";
+export interface IUser {
+  username: string;
+  password: string;
+  email: string;
+  firstname: string;
+  lastname: string;
+  age: number;
+  city: string;
+  work: string;
 }
 
-export default SignUp
+const SignUp = () => {
+  const [user, setUser] = useState<IUser | null>({
+    username: "",
+    password: "",
+    email: "",
+    firstname: "",
+    lastname: "",
+    age: 0,
+    city: "",
+    work: "",
+  });
+
+  function SignUp(e) {
+    e.preventDefault();
+    axios.post(`${baseURL}/api/users/signup`, user).then((res) => {
+      if (res.data !== "") {
+        window.location.href = "/login";
+      } else {
+        alert("Wrong information!");
+      }
+    });
+  }
+
+  function handle(e) {
+    //@ts-ignore
+    setUser({ ...user, [e.target.id]: e.target.value });
+  }
+
+  return (
+    <div>
+      <h1>Sign Up</h1>
+      <form>
+        <h4>
+          Username:
+          <input
+            type="text"
+            id="username"
+            placeholder="Enter Username:"
+            onChange={handle}
+            value={user?.username}
+          />
+          <br />
+          <br />
+          Password:
+          <input
+            type="password"
+            id="password"
+            placeholder="Enter Password:"
+            onChange={handle}
+            value={user?.password}
+          />
+          <br />
+          <br />
+          Password again:
+          <input
+            type="password"
+            id="repass"
+            placeholder="Enter Password Again:"
+            onChange={handle}
+          />
+          <br />
+          <br />
+          Email Address:
+          <input
+            type="text"
+            id="email"
+            placeholder="Enter Email Address:"
+            onChange={handle}
+            value={user?.email}
+          />
+          <br />
+          <br />
+          First Name:
+          <input
+            type="text"
+            id="fName"
+            placeholder="Enter First Name:"
+            onChange={handle}
+            value={user?.firstname}
+          />
+          <br />
+          <br />
+          Last Name:
+          <input
+            type="text"
+            id="lName"
+            placeholder="Enter Last Name:"
+            onChange={handle}
+            value={user?.lastname}
+          />
+          <br />
+          <br />
+          Birth Date:
+          <input type="date" id="age" onChange={handle} value={user?.age} />
+          <br />
+          <br />
+          City:
+          <input
+            type="text"
+            id="address"
+            placeholder="Enter City:"
+            onChange={handle}
+            value={user?.city}
+          />
+          <br />
+          <br />
+          Place Of Work:
+          <input
+            type="text"
+            id="address"
+            placeholder="Enter Place Of Work:"
+            onChange={handle}
+            value={user?.work}
+          />
+          <br />
+          <br />
+          <button onClick={SignUp}>Submit</button>
+        </h4>
+      </form>
+    </div>
+  );
+};
+
+export default SignUp;
