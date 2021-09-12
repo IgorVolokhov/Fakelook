0c118e16b58d33cb2fa71a9a38ac710e17125ee0
eyalrainit <eyalrainitz@gmail.com>
2021-09-13 02:03:00 +0300

    add Date

--- react/src/components/Options.tsx
+++ react/src/components/Options.tsx
@@ -1,14 +1,69 @@
 import CustomButton from "../models/CustomButton";
+import Grid from '@material-ui/core/Grid';
+import React,  {useState} from 'react';
+import DateFnsUtils from '@date-io/date-fns';
+import {
+  MuiPickersUtilsProvider,
+  KeyboardTimePicker,
+  KeyboardDatePicker,
+} from '@material-ui/pickers';
+import 'date-fns';
+import { TextField } from "@material-ui/core";
 
 interface Props {
   addPost: any;
 }
-
+//TO DO axios
+//Add data to array to choose with push 
 const Options = ({ addPost }: Props) => {
+  const [firstSelectedDate, setFirstSelectedDate] = useState<Date | null>(
+    new Date(Date.now()),
+  );
+  const [secondSelectedDate, setSecondSelectedDate] = useState<Date | null>(
+    new Date(Date.now()),
+  );
+  const firsthandleDateChange = (date: Date | null) => {
+    setFirstSelectedDate(date);
+  };
+  const secondhandleDateChange = (date: Date | null) => {
+    setSecondSelectedDate(date);
+  };
   return (
     <div>
       Options: my friends(button) manage posts (button) filter (options to
       filter)
+      <MuiPickersUtilsProvider utils={DateFnsUtils}>
+      <Grid container justifyContent="space-around">
+        <KeyboardDatePicker
+          disableToolbar
+          variant="inline"
+          format="dd/MM/yyyy"
+          margin="normal"
+          id="date-picker-inline"
+          label="From"
+          value={firstSelectedDate}
+          onChange={firsthandleDateChange}
+          KeyboardButtonProps={{
+            'aria-label': 'change date',
+          }}
+        />
+        <KeyboardDatePicker
+          disableToolbar
+          variant="inline"
+          format="dd/MM/yyyy"
+          margin="normal"
+          id="date-picker-inline"
+          label="To"
+          value={secondSelectedDate}
+          onChange={secondhandleDateChange}
+          KeyboardButtonProps={{
+            'aria-label': 'change date',
+          }}
+        />
+         </Grid>
+        </MuiPickersUtilsProvider>
+
+        <TextField label="Radious from you"/>
       <div>
         {/*show my posts*/}
         <CustomButton text="My Posts" />
