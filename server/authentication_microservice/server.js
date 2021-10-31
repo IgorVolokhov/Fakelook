eb870395d3957a5c7a597298e4bd95894c0d0eb8
IgorVolokhov <volohkov9@gmail.com>
2021-10-31 10:20:53 +0200

    looking for url

--- server/authentication_microservice/server.js
+++ server/authentication_microservice/server.js
@@ -1,9 +1,9 @@
 require("dotenv").config();
 const http = require("http");
 const app = require("./app");
-const port = process.env.PORT_AUTHENTICATION;
-const url = process.env.URL_AUTHENTICATION;
+// const port = process.env.PORT_AUTHENTICATION;
+const port = process.env.PORT || process.env.PORT_AUTHENTICATION;
 
 const server = http.createServer(app);
 
-server.listen(url, () => console.log(`run on url: ${url}`));
+server.listen(port, () => console.log(`run on port: ${port}`));
