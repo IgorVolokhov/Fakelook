411824d65b22903d6e842db01a6899605cbccce9
IgorVolokhov <volohkov9@gmail.com>
2021-10-31 09:36:55 +0200

    updated .env and url for cloud

--- server/authentication_microservice/server.js
+++ server/authentication_microservice/server.js
@@ -2,9 +2,8 @@ require("dotenv").config();
 const http = require("http");
 const app = require("./app");
 const port = process.env.PORT_AUTHENTICATION;
+const url = process.env.URL_AUTHENTICATION;
 
 const server = http.createServer(app);
 
-server.listen(port, () => console.log(`run on http://localhost:${port}`));
-
-// TODO GO OVER EACH .ENV SEE WHAT I CAN REMOVE
+server.listen(url, () => console.log(`run on url: ${url}`));
